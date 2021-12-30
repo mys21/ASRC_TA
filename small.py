78050a4e0155a1a37c0f6dafbdf97ff3bb0ad0c3
annezats <anne.zats70@myhunter.cuny.edu>
2021-12-30 05:07:43 -0500

    plots pump on and off now

--- small.py
+++ small.py
@@ -2,7 +2,8 @@ from PyQt5 import QtGui, QtCore,QtWidgets
 from small_gui_class import Ui_MainWindow
 import sys
 from small_camera import camera1
-from TA2_camera import * #octoplus
+from TA2_camera import *
+from ta_data_processing_class import *
 
 class Editor(QtWidgets.QMainWindow):
 
@@ -15,7 +16,7 @@ class Editor(QtWidgets.QMainWindow):
 
         #connects buttons to functions
         self.ui.initializeButton.clicked.connect(self.exec_initialize_btn)
-        self.ui.linePeriod.textChanged.connect(self.update_line_period)
+        self.ui.linePeriod.textChanged.connect(self.update_lines_per_frame)
 
         #sets variables
         self.ui.History.setText(" ")
@@ -24,39 +25,82 @@ class Editor(QtWidgets.QMainWindow):
     def append_history(self,message):
         self.ui.History.append(message)
 
-    def update_line_period(self):
+    def update_lines_per_frame(self):
         '''execute on text typed - updates line period'''
 
         try:
             self.lines_per_frame= int(self.ui.linePeriod.text())
-            self.append_history("changed line period to: " + str(self.line_period))
+            self.append_history("changed line period to: " + str(self.lines_per_frame))
         except:
             self.append_history("that is not an integer, try again")
-
         return
 
     #things that happen during runtime
     def exec_initialize_btn(self):
         '''execute on pushed button - initializes camera'''
         self.append_history("button pushed")
-        self.camera= octolpus()
+        self.camera= octoplus()
         self.camera.Initialize( lines_per_frame=self.lines_per_frame)
         #self.cameraLog = "\n".join(self.camera.log)
         self.append_history("camera initialized")
+        self.append_history("Number of Cameras: "+ str(self.camera.ulNbCameras.value))
         self.camera.Acquire()
         self.append_history("data acquired")
-        plotting()
+        self.append_history("Buffer Size: "+ str(oc.ImageInfos.iBufferSize))
+        self.processing()
+        self.append_history("data processed")
+        self.create_plots()
         self.append_history("plots created")
+        self.camera.Exit()
         return
 
-    def plotting(self):
-
-        pass
+    def processing(self):
+        self.ta = ta_data_processing(self.camera.probe, self.camera.reference, self.camera.first_pixel, self.camera.num_pixels)
+    	self.ta.separate_on_off()
+    	self.ta.average_shots() #NOTE: average shots can only be called after separate_on_off
+        #self.append_history("Pump on: ", self.ta.probe_on) #not sure if this is good to append cause entire array
+    	#self.append_history("Pump off: ", self.ta.probe_off)
         return
+
+    #--start of plotting functions---
+    def create_plots(self):
+        #in self.ui.plotPumpOn, plot self.ta.probe_on because thats not confusing at all...
+    	#in self.ui.plotPumpOff, plot self.ta.probe_off because thats not confusing at all...
+        self.num_pixels= self.camera.num_pixels
+        self.plot_waves = np.linspace(0,self.num_pixels-1,self.num_pixels)
+        pump_off_plot()
+        pump_on_plot()
+        return
+
+    def pump_off_plot(self):
+        '''pump off plot'''
+        try:
+            self.ui.plotPumpOff.plotItem.plot(self.plot_waves,self.ta.probe_off,pen='g',clear=True)
+        except:
+            self.append_history('Error Plotting pump off Plot')
+
+        self.ui.plotPumpOff.plotItem.setLabels(left='dtt',bottom='Wavelength / Pixel')
+        self.ui.plotPumpOff.plotItem.showAxis('top',show=True)
+        self.ui.plotPumpOff.plotItem.showAxis('right',show=True)
+        return
+
+    def pump_on_plot(self):
+        '''pump off plot'''
+        try:
+            self.ui.plotPumpOn.plotItem.plot(self.plot_waves,self.ta.probe_on,pen='g',clear=True)
+        except:
+            self.append_history('Error Plotting pump on Plot')
+
+        self.ui.plotPumpOn.plotItem.setLabels(left='dtt',bottom='Wavelength / Pixel')
+        self.ui.plotPumpOn.plotItem.showAxis('top',show=True)
+        self.ui.plotPumpOn.plotItem.showAxis('right',show=True)
+        return
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     ex = Editor()
     ex.show()
+    #ex.create_plots()
     sys.exit(app.exec_())
 
 
